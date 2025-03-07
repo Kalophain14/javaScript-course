eac415a6ad4f1977060e9555240572f8a8a27492
‘Kalophain14’ <chueutemosho@gmail.com>
2025-03-07 21:13:34 +0200

    Initial commit

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,17 @@
+//Values and Variables #1
+
+let country = 'South Africa';
+let continent = 'Africa';
+let population = 100033737;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let province = "Western Cape";
+let city = "Cape Town"
+let thePopulation = 27373;
+
+console.log(province);
+console.log(city);
+console.log(thePopulation);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,31 @@
+/* let js = "amazing";
+if (js === "amazing") alert("JavaScript is FUN!");
+
+48 + 8 + 23 - 10;
+console.log(40 + 8 + 23 - 10); */
+
+// Values and Variables
+/*let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas"); 
+console.log(23);
+
+let firstName = "Temoso";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let $function = 11;
+let name = "Temoso"; // dont use often
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
+*/
+
+// Data Types
+
