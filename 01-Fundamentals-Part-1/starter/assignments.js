421702f3eb17c2a66b0599820760f01643f003a8
‘Kalophain14’ <chueutemosho@gmail.com>
2025-03-13 20:14:25 +0200

    New assignment based on boolean, variables, operators and their precenders

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,7 +1,7 @@
 //Values and Variables #1
 
-let country = 'South Africa';
-let continent = 'Africa';
+let country = "South Africa";
+let continent = "Africa";
 let population = 100033737;
 
 console.log(country);
@@ -9,9 +9,49 @@ console.log(continent);
 console.log(population);
 
 let province = "Western Cape";
-let city = "Cape Town"
+let city = "Cape Town";
 let thePopulation = 27373;
 
 console.log(province);
 console.log(city);
 console.log(thePopulation);
+
+// Operators, Variable assignment
+
+/* Write your code below. Good luck! 🙂 */
+
+//Data1
+let massMark = 78,
+  heightMark = 1.69,
+  massJohn = 92,
+  heightJohn = 1.95;
+
+let BMIMark = massJohn / (heightMark * heightMark);
+console.log(BMIMark);
+
+let BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIJohn);
+
+//Data2
+let massMark2 = 95,
+  heightMark2 = 1.88,
+  massJohn2 = 85,
+  heightJohn2 = 1.76;
+
+let BMIMark2 = massJohn2 / (heightMark2 * heightMark2);
+console.log(BMIMark2);
+
+let BMIJohn2 = massJohn2 / (heightJohn2 * heightJohn2);
+console.log(BMIJohn2);
+
+//Boolean statement
+const BMIBoolean1 = massJohn < massMark;
+
+console.log(BMIJohn, BMIMark, BMIBoolean1);
+console.log(
+  "is the mass of massJohn less than the mass of massMark? " + BMIBoolean1
+);
+
+const BMIBoolean2 = massJohn2 < massMark2;
+console.log(BMIJohn2, BMIMark2, BMIBoolean2);
+console.log("is the mass of john less than the mass of mark? " + BMIBoolean2);
