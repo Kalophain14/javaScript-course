5ad11cb431c7969ffa7743b305adefbf3d605f2c
‘Kalophain14’ <chueutemosho@gmail.com>
2025-03-31 19:25:24 +0200

    Adding ignored files

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -55,3 +55,22 @@ console.log(BMIJohn2);
 const BMIBoolean2 = massJohn2 < massMark2;
 console.log(BMIJohn2, BMIMark2, BMIBoolean2);
 console.log("is the mass of john less than the mass of mark? " + BMIBoolean2);
+
+// Assignment 2
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+/* Write your code below. Good luck! 🙂 */
+
+if (BMIMark > BMIJohn) {
+  console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+} else {
+  console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+}
