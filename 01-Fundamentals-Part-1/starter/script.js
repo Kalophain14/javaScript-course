08cae7f3e17cb6c8d505060b6e50ad113cdee229
‘Kalophain14’ <chueutemosho@gmail.com>
2025-03-31 20:50:17 +0200

    Truthy and Falsy values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -216,3 +216,33 @@ let n = `1` + 1; //string 11 it is concatenated + 1
 n = n - 1;
 console.log(n);
 */
+
+// Truthy and Falsy values
+
+// 5 falsy values
+// 0;
+// `` = empty string
+// undefined
+// null
+// NaN = Not a number
+
+console.log(Boolean(0));
+console.log(Boolean(`Jonas`));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(`j`));
+
+const money = 100;
+
+if (money) {
+  console.log(`Don't spend it all 😉`);
+} else {
+  console.log(`You should get a job!`);
+}
+
+let height = 1.92;
+if (height) {
+  console.log(`YAY Height is defined`);
+} else {
+  console.log(`Height is UNDEFINED`);
+}
